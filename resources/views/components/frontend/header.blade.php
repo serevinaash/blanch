d94cf5e7ea31a161108f148739f6a44f3171e273
serevinaash <serevinasherly02@gmail.com>
2023-12-24 10:28:03 +0700

    index

---
+++ resources/views/components/frontend/header.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8" />
+    <title>LuxSpace</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:image" content="" />
+
+    <link rel="manifest" href="site.webmanifest" />
+    <link rel="apple-touch-icon" href={{url("/frontend/images/content/favicon.png")}} />
+    <!-- Place favicon.ico in the root directory -->
+
+    {{-- <link rel="stylesheet" href="{{url("/frontend/css/main.css")}}" /> --}}
+    <link rel="icon" href={{url("/frontend/images/content/favicon.png")}} />
+
+    <meta name="theme-color" content="#000" />
+    <link rel="icon" href="{{url("/frontend/favicon.ico")}}">
+    <link href="{{url("/frontend/css/app.minify.css")}}" rel="stylesheet">
+   </head>
+   
+   <body>
